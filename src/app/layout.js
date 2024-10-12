e6ae5afa95ea54dd27872dd030b13e7afeade103
Rohan Wandre <rohanwandre24@gmail.com>
2024-10-12 23:06:18 +0530

    added google ads

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,22 +1,27 @@
-"use client"
+"use client";
 
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { AuthContext } from '../../contexts/AuthContext'
+import { AuthContext } from "../../contexts/AuthContext";
 import { useState } from "react";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
-  const [admin, setAdmin] = useState(false)
-  const [villa, setVilla] = useState(false)
+  const [admin, setAdmin] = useState(false);
+  const [villa, setVilla] = useState(false);
 
   return (
     <html lang="en">
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5462977881633609"
+        crossorigin="anonymous"
+      />
       <AuthContext.Provider value={{ admin, setAdmin, villa, setVilla }}>
         <body className={inter.className}>{children}</body>
       </AuthContext.Provider>
-
     </html>
   );
 }
